9b42e7d0c15acb2c1678cd489bcec154d6c6c031
Keir Whitlock <keir@whitlock.it>
2019-10-25 12:42:22 +0100

    Running terraform fmt

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,19 +1,19 @@
 resource "aws_dynamodb_table" "books" {
-	name	= "Books"
-	hash_key = "ISBN"
-	attribute {
-		name = "ISBN"
-		type = "S"
-	}
-	read_capacity = 5
-	write_capacity = 5
+  name     = "Books"
+  hash_key = "ISBN"
+  attribute {
+    name = "ISBN"
+    type = "S"
+  }
+  read_capacity  = 5
+  write_capacity = 5
 }
-	
-resource "aws_dynamodb_table_item" "testBookOne" {
-	table_name = "${aws_dynamodb_table.books.name}"
-	hash_key = "${aws_dynamodb_table.books.hash_key}"
 
-	item = <<ITEM
+resource "aws_dynamodb_table_item" "testBookOne" {
+  table_name = "${aws_dynamodb_table.books.name}"
+  hash_key   = "${aws_dynamodb_table.books.hash_key}"
+
+  item = <<ITEM
 {
 	"ISBN": {"S": "978-1420931693"},
 	"Title": {"S": "The Republic"},
@@ -23,10 +23,10 @@ ITEM
 }
 
 resource "aws_dynamodb_table_item" "testBookTwo" {
-	table_name = "${aws_dynamodb_table.books.name}"
-	hash_key = "${aws_dynamodb_table.books.hash_key}"
+  table_name = "${aws_dynamodb_table.books.name}"
+  hash_key   = "${aws_dynamodb_table.books.hash_key}"
 
-	item = <<ITEM
+  item = <<ITEM
 {
 	"ISBN": {"S": "978-0486298238"},
 	"Title": {"S": "Meditations"},
--- terraform/lambda_func.tf
+++ terraform/lambda_func.tf
@@ -1,11 +1,11 @@
 resource "aws_lambda_function" "books" {
-	filename = "main.zip"
-	function_name = "books"
-	role = "${aws_iam_role.lambda-books-executor.arn}"
-	handler = "main"
+  filename      = "main.zip"
+  function_name = "books"
+  role          = "${aws_iam_role.lambda-books-executor.arn}"
+  handler       = "main"
 
-	source_code_hash = "${filebase64sha256("main.zip")}"
-	runtime = "go1.x"
+  source_code_hash = "${filebase64sha256("main.zip")}"
+  runtime          = "go1.x"
 }
 
 resource "aws_iam_role" "lambda-books-executor" {
@@ -29,10 +29,10 @@ EOF
 }
 
 resource "aws_iam_role_policy" "lambda-books-executor" {
-	name = "dynamodb-item-crud-role"
-	role = "${aws_iam_role.lambda-books-executor.id}"
+  name = "dynamodb-item-crud-role"
+  role = "${aws_iam_role.lambda-books-executor.id}"
 
-	policy = <<EOF
+  policy = <<EOF
 {
     "Version": "2012-10-17",
     "Statement": [
@@ -53,9 +53,9 @@ EOF
 }
 
 resource "aws_lambda_permission" "allow_books_apigateway" {
-	statement_id = "AllowExecutionFromBooksAPIGateway"
-	action = "lambda:InvokeFunction"
-	function_name = "${aws_lambda_function.books.function_name}"
-	principal = "apigateway.amazonaws.com"
-	source_arn = "arn:aws:execute-api:${var.region}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.bookstore.id}/*/*/*"
+  statement_id  = "AllowExecutionFromBooksAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.books.function_name}"
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "arn:aws:execute-api:${var.region}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.bookstore.id}/*/*/*"
 }
